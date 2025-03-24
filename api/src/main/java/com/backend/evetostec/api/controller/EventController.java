80648b4b7ad02fa94d40698c2060d3af956235ff
saraahkarolline <saraahkarolline@gmail.com>
2025-03-24 20:59:18 -0300

    controller

---
+++ api/src/main/java/com/backend/evetostec/api/controller/EventController.java
@@ -0,0 +1,23 @@
+package com.backend.evetostec.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.evetostec.api.domain.event.Event;
+import com.backend.evetostec.api.domain.event.EventRequestDTO;
+import com.backend.evetostec.api.service.EventService;
+
+@RestController
+@RequestMapping("api/event")
+public class EventController {
+    @Autowired
+    private EventService eventService;
+
+    public ResponseEntity<Event> create(@RequestBody EventRequestDTO body){
+        Event newEvent = this.eventService.createEvent(body);
+        return ResponseEntity.ok(newEvent);
+    }
+}
